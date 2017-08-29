6ba109b381a664cd3cb2a042a3218effcebedc8c
Brian So <briansostudio@gmail.com>
2017-08-29 14:04:45 +0800

    add zoom

--- dev-src/line-chart/LineChart.vue
+++ dev-src/line-chart/LineChart.vue
@@ -141,14 +141,37 @@
       mc.add([pinch, pan]);
 
       let lastEv;
+      let lastPointers;
       mc.on("pinch pan", (ev) => {
 
+        ev.pointers = [...ev.pointers];
         console.log(ev);
+
         if(lastEv){
+
+
           this.chart.scrollInPx({
-            x:-(ev.deltaX - lastEv.deltaX),
-            y:(ev.deltaY - lastEv.deltaY)
+            x:-(ev.center.x - lastEv.center.x),
+            y:(ev.center.y - lastEv.center.y)
           });
+
+          if(ev.pointers.length > 1){
+            //pinch
+            if(lastPointers){
+              const thisDistanceX = ev.pointers[0].clientX - ev.pointers[1].clientX;
+              const lastDistanceX = lastPointers[0].clientX - lastPointers[1].clientX;
+
+              const thisDistanceY = ev.pointers[0].clientY - ev.pointers[1].clientY;
+              const lastDistanceY = lastPointers[0].clientY - lastPointers[1].clientY;
+
+              this.chart.zoom({
+                x:Math.abs(lastDistanceX/thisDistanceX),
+                y:Math.abs(lastDistanceY/thisDistanceY)
+              });
+            }
+            lastPointers = ev.pointers;
+          }
+
         }else{
           this.chart.scrollInPx({
             x:-ev.deltaX,
@@ -161,6 +184,7 @@
       });
       mc.on("panend pinchend", (ev)=>{
         lastEv = undefined;
+        lastPointers = undefined;
       });
     },
     beforeDestroy(){
